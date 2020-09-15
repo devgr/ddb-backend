70332ef5a169014cfc9289e6d51c81d8c0962de3
George Darling <devgr4@gmail.com>
2020-09-15 15:39:11 -0500

    Added swashbuckle swagger doc

--- Ddb.Api/Startup.cs
+++ Ddb.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,7 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 using Ddb.Application.Abstractions;
 using Ddb.Application.Services;
 using Ddb.Infrastructure.Repositories;
@@ -42,11 +44,33 @@ namespace Ddb.Api
 
             // Add application services
             services.AddSingleton<IApplicationService, ApplicationService>();
+
+            // Add Swagger Doc
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "DDB API",
+                        Version = "v1"
+                    }
+                );
+
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "Ddb.Api.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Add Swagger Doc
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DDB API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
